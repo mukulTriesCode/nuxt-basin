c774e55f58f7bbaef777c54cc27be96c5d235b45
Mukul Sharma <mukulsharma262002@gmail.com>
2024-11-22 16:27:10 +0530

    Added yup validation

--- components/SCForm.vue
+++ components/SCForm.vue
@@ -1,3 +1,82 @@
+<script setup>
+import { ref, onMounted } from 'vue';
+import { useRoute } from 'vue-router';
+import { useRuntimeConfig } from 'nuxt/app';
+import { RecaptchaV2 } from 'vue3-recaptcha-v2';
+import * as Yup from 'yup';
+
+const route = useRoute();
+const config = useRuntimeConfig();
+// UTM parameters
+const UtmValues = {
+    utm_id: route.query?.utm_id,
+    utm_source: route.query?.utm_source,
+    utm_medium: route.query?.utm_medium,
+    utm_campaign: route.query?.utm_campaign,
+    utm_content: route.query?.utm_content,
+    utm_term: route.query?.utm_term,
+};
+
+const targetURL = ref(`${config.public.HOSTNAME}${route.path}`);
+// const SITEKEY = '6LfzuIYqAAAAAA5cdWDhmrBONdXfOP2ZlKP7vTbZ'; // reCaptcha v3 site key
+const SITEKEY = '6Ley44YqAAAAACGTSLIgfgUqLswCe-tSmkSsYAuN'; // reCaptcha v2 site key
+const data = ref({
+    note: 'Secure form. Your information is confidential with us.',
+    submitBtnText: 'Review my case'
+});
+
+const formSchema = Yup.object().shape({
+    name: Yup.string().required('Name is required'),
+    email: Yup.string().email('Invalid email').required('Email is required'),
+    tel: Yup.string().required('Phone is required'),
+    message: Yup.string().required('Message is required'),
+    recaptcha: Yup.string().required('reCaptcha validation is required'),
+});
+
+const onSubmit = async (e) => {
+    // Add your form submission logic here
+    e.preventDefault();
+    
+    try {
+        const formData = new FormData(document.getElementById('myForm'));
+        const formValues = Object.fromEntries(formData.entries());
+        const isValid = await formSchema.validate(formValues, { abortEarly: false });
+        if (!isValid) {
+            console.error('Form validation failed');
+            return;
+        }
+        const response = await fetch('https://usebasin.com/f/2d02e5938cc2', {
+            method: 'POST',
+            body: formData,
+        });
+        if (!response.ok) {
+            throw new Error('Failed to submit form');
+        }
+        console.log('Form submitted successfully');
+        // Reset the form fields
+        document.getElementById('myForm').reset();
+    } catch (error) {
+        console.error('Error submitting form:', error);
+    }
+}
+
+// Load reCaptcha script dynamically
+onMounted(() => {
+    // Dynamically load reCaptcha script if not already loaded
+    if (typeof grecaptcha === 'undefined') {
+        const script = document.createElement('script');
+        script.src = `https://www.google.com/recaptcha/api.js?render=${SITEKEY}`;
+        script.async = true;
+        script.defer = true;
+        script.onload = () => {
+            console.log('reCaptcha script loaded successfully');
+        };
+        document.head.appendChild(script);
+    }
+});
+</script>
+
+
 <template>
     <form id="myForm" @submit="onSubmit" class="form" method="POST" name="Review_My_Case_Form" ref="ReviewForm"
         data-basin-spam-protection='recaptcha'>
@@ -23,77 +102,3 @@
         </div>
     </form>
 </template>
-
-<script setup>
-import { ref, onMounted } from 'vue';
-import { useRoute } from 'vue-router';
-import { useRuntimeConfig } from 'nuxt/app';
-import { RecaptchaV2 } from 'vue3-recaptcha-v2';
-
-const route = useRoute();
-const config = useRuntimeConfig();
-// UTM parameters
-const UtmValues = {
-    utm_id: route.query?.utm_id,
-    utm_source: route.query?.utm_source,
-    utm_medium: route.query?.utm_medium,
-    utm_campaign: route.query?.utm_campaign,
-    utm_content: route.query?.utm_content,
-    utm_term: route.query?.utm_term,
-};
-
-const targetURL = ref(`${config.public.HOSTNAME}${route.path}`);
-// const SITEKEY = '6LfzuIYqAAAAAA5cdWDhmrBONdXfOP2ZlKP7vTbZ'; // reCaptcha v3 site key
-const SITEKEY = '6Ley44YqAAAAACGTSLIgfgUqLswCe-tSmkSsYAuN'; // reCaptcha v2 site key
-const data = ref({
-    note: 'Secure form. Your information is confidential with us.',
-    submitBtnText: 'Review my case'
-});
-
-const onSubmit = () => {
-    // Add your form submission logic here
-    console.log('Form submitted successfully');
-    // Reset the form fields
-    //...
-}
-
-// Load reCaptcha script dynamically
-onMounted(() => {
-    // Dynamically load reCaptcha script if not already loaded
-    if (typeof grecaptcha === 'undefined') {
-        const script = document.createElement('script');
-        script.src = `https://www.google.com/recaptcha/api.js?render=${SITEKEY}`;
-        script.async = true;
-        script.defer = true;
-        script.onload = () => {
-            console.log('reCaptcha script loaded successfully');
-        };
-        document.head.appendChild(script);
-    }
-
-    const myForm = document.getElementById('myForm');
-    if (myForm) {
-        myForm.addEventListener('submit', function (event) {
-            event.preventDefault(); // Prevent the default form submission
-
-            // Ensure grecaptcha is ready before submitting the form
-            if (typeof grecaptcha !== 'undefined') {
-                grecaptcha.ready(function () {
-                    grecaptcha.execute(SITEKEY, { action: 'submit' }).then(function (token) {
-                        // Set the token in the hidden field
-                        document.getElementById('g-recaptcha-response').value = token;
-
-                        // Now submit the form
-                        myForm.submit();
-                    }).catch((err) => {
-                        console.error('reCaptcha error: ', err);
-                        // Handle the error (maybe show a message to the user)
-                    });
-                });
-            } else {
-                console.error('reCaptcha is not defined yet!');
-            }
-        });
-    }
-});
-</script>

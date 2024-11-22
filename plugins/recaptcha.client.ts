5ca1e20f4132e37bb46682918c4364195f18aa29
Mukul Sharma <mukulsharma262002@gmail.com>
2024-11-22 16:11:19 +0530

    Updated the captcha sitekey

--- plugins/recaptcha.client.ts
+++ plugins/recaptcha.client.ts
@@ -2,7 +2,7 @@ import { install } from "vue3-recaptcha-v2";
 
 export default defineNuxtPlugin((nuxtApp) => {
   nuxtApp.vueApp.use(install, {
-    sitekey: "6LeowoYqAAAAAOcohG80sFTSfIH_OKbHzpjZn5na",
+    sitekey: "6Ley44YqAAAAACGTSLIgfgUqLswCe-tSmkSsYAuN",
     cnDomains: false,
   });
 });
